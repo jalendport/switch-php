e56e7cf67fb3e3b4db1495eb3755de39578685e9
jalendport <jalen@dominion-designs.com>
2018-07-02 11:06:43 -0400

    Updated Brew PHP package names

--- switch-php.sh
+++ switch-php.sh
@@ -14,8 +14,8 @@ NC="\e[0m" # Reset everything
 
 
 # Currently available php packages
-php_array=("php56" "php70" "php71" "php72")
-brew_array=("56" "70" "71" "72")
+brew_array=("5.6" "7.0" "7.1" "7.2")
+php_array=("php@5.6" "php@7.0" "php@7.1" "php@7.2")
 
 
 # Starts the spinner
@@ -84,9 +84,9 @@ show_help() {
 	printf "\n${YELLOW}Customizing the PHP Memory Settings:${NC}\n"
 	printf "  - If you don't pass an argument to \"-m\" or \"--memory\", it will reset any previously set custom memory settings to the default Valet config.\n"
 	printf "  - Alternatively, you can pass an argument to \"-m\" or \"--memory\" if you want to override the default Valet memory settings. For example, you can do: \n\n"
-	printf "      ${GREEN}switch-php 71 -m 512M${NC}       # php71 with 512MB of memory\n"
-	printf "      ${GREEN}switch-php 72 -m 2G -v${NC}      # php72 with 2GB of memory; verbose output\n"
-	printf "      ${GREEN}switch-php 56 --memory=1G${NC}   # php56 with 1GB of memory\n\n"
+	printf "      ${GREEN}switch-php 7.1 -m 512M${NC}       # php@7.1 with 512MB of memory\n"
+	printf "      ${GREEN}switch-php 7.2 -m 2G -v${NC}      # php@7.2 with 2GB of memory; verbose output\n"
+	printf "      ${GREEN}switch-php 5.6 --memory=1G${NC}   # php@5.6 with 1GB of memory\n\n"
 	printf "  - Note: customizing PHP memory settings currently only works for Laravel Valet users. If you don't use Valet, we hope to get this working for you as well in an upcoming release.\n"
 	exit
 
@@ -106,9 +106,8 @@ while :; do
             show_help
             exit
             ;;
-		56|70|71|72) # If a version is specified; then
-			php_version="php$1"
-			php_version_sh="${php_version:3:1}.${php_version:4}"
+		5.6|7.0|7.1|7.2) # If a version is specified; then
+			php_version="php@$1"
 			rflag="true"  # Required!
 			;;
 	    -v|--verbose) # The verbose option
@@ -183,7 +182,7 @@ if [[ " ${php_installed_array[*]} " == *"$php_version"* ]]; then # If the reques
 			fi
 		done
 		[ $verbose -eq 1 ] && printf " ==>  Linking $php_version...\n" # If $verbose, then echo
-		brew link "$php_version" &> /dev/null # Link the new PHP version and hide the output
+		brew link --force "$php_version" &> /dev/null # Link the new PHP version and hide the output
 		[ $verbose -eq 1 ] && printf " ==>  Starting $php_version...\n" # If $verbose, then echo
 		brew services start "$php_version" &> /dev/null # Start the Brew service for the new PHP version and hide the output
 	[ $verbose -eq 1 ] && stop_spinner " ✅  PHP switched" || stop_spinner "PHP switched" # If $verbose, then echo a; otherwise, echo b
